e7611e458f025dcc59811932169231ff845e734a
Roman Divkovic <Roman.Divkovic@Capio.se>
2023-08-18 14:45:15 +0200

    Add routes and users routes to create/delete/update/fins etc..

--- server.js
+++ server.js
@@ -2,7 +2,7 @@ const express = require('express')
 const bodyParser = require('body-parser')
 const mongoose = require('mongoose')
 
-// Importera modeller
+// Importera modeller (du kommer fortfarande behöva dessa för andra delar av din app)
 const User = require('./Models/User')
 const Household = require('./Models/Household')
 const Pet = require('./Models/Pet')
@@ -11,27 +11,18 @@ const app = express()
 
 app.use(bodyParser.json())
 
-// TODO: Anslut till din MongoDB databas
+// Anslut till din MongoDB databas
 mongoose.connect('mongodb://localhost:27017/mydatabase', {
   useNewUrlParser: true,
   useUnifiedTopology: true
 })
 
-// API endpoints
-// Här kan du lägga till endpoints för att skapa, läsa, uppdatera och radera användare, hushåll och husdjur.
+// Importera och använd dina routes
+const userRoutes = require('./routes/userRoutes')
+app.use('/users', userRoutes)
 
 // Starta servern
 const PORT = 3000
 app.listen(PORT, () => {
   console.log(`Server running on port ${PORT}`)
-})
-
-app.post('/users', async (req, res) => {
-  try {
-    const newUser = new User(req.body)
-    const savedUser = await newUser.save()
-    res.json(savedUser)
-  } catch (error) {
-    res.status(500).json({ message: error.message })
-  }
 })
